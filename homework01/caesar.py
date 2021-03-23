33305cbece6454b757db37f38840bd1a54ecd9cd
Анастасия <az20022002@icloud.com>
2021-03-23 12:35:03 +0300

    Исправлены замечания

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -16,22 +16,21 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
-    for i in plaintext:
-        if i == " ":
-            ciphertext += " "
-        else:
-            f = string.ascii_lowercase.find(i)
-            if f == -1:
-                f = string.ascii_uppercase.find(i)
-                if f == -1:
-                    ciphertext += i
+    for index in plaintext:
+        if ord("z") >= ord(index) >= ord("a") or ord("Z") >= ord(index) >= ord("A"):
+            ch = string.ascii_lowercase.find(index)
+            if ch == -1:
+                ch = string.ascii_uppercase.find(index)
+                if ch == -1:
+                    ciphertext += index
                 else:
-                    res = (f + shift) % len(string.ascii_uppercase)
+                    res = (ch + shift) % len(string.ascii_uppercase)
                     ciphertext += string.ascii_uppercase[res]
             else:
-                res = (f + shift) % len(string.ascii_lowercase)
+                res = (ch + shift) % len(string.ascii_lowercase)
                 ciphertext += string.ascii_lowercase[res]
+        else:
+            ciphertext += index
     return ciphertext
 
 
@@ -49,22 +48,21 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
-    for i in ciphertext:
-        if i == " ":
-            plaintext += " "
-        else:
-            f = string.ascii_lowercase.find(i)
-            if f == -1:
-                f = string.ascii_uppercase.find(i)
-                if f == -1:
-                    plaintext += i
+    for index in ciphertext:
+        if ord("z") >= ord(index) >= ord("a") or ord("Z") >= ord(index) >= ord("A"):
+            ch = string.ascii_lowercase.find(index)
+            if ch == -1:
+                ch = string.ascii_uppercase.find(index)
+                if ch == -1:
+                    plaintext += index
                 else:
-                    res = (f - shift) % len(string.ascii_uppercase)
+                    res = (ch - shift) % len(string.ascii_uppercase)
                     plaintext += string.ascii_uppercase[res]
             else:
-                res = (f - shift) % len(string.ascii_lowercase)
+                res = (ch - shift) % len(string.ascii_lowercase)
                 plaintext += string.ascii_lowercase[res]
+        else:
+            plaintext += index
     return plaintext
 
 
@@ -73,14 +71,13 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
     k = 0
     for shift in range(26):
         s = 0
         plaintext = decrypt_caesar(ciphertext, shift)
-        mm = plaintext.split()
-        for m in mm:
-            if m in dictionary:
+        words = plaintext.split()
+        for word in words:
+            if word in dictionary:
                 s += 1
             if s > k:
                 k = s
